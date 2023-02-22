6c664d85d6a2b8d076528d78aada30fca08418f6
cleevrl <cleevrl@gmail.com>
2023-02-22 18:36:03 +0900

    fix: Fix script to copy extlinux.conf correctly

--- boot-ext-sdcard/set_ext_sdcard.sh
+++ boot-ext-sdcard/set_ext_sdcard.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Target Model : Jetson Nano 4G P3448
-# Test with Flash writing Jetpack 4.6.1
+# Test with Flash writing Jetpack 4.6.3
+# Need init-tools dir exist ~/ for good work
 
 cd ~
 git clone https://github.com/Seeed-Studio/seeed-linux-dtoverlays.git
@@ -14,4 +15,4 @@ cd /boot/
 echo "dongbuict0" | sudo -S /opt/nvidia/jetson-io/config-by-hardware.py -l
 echo "dongbuict0" | sudo -S /opt/nvidia/jetson-io/config-by-hardware.py -n "reComputer sdmmc"
 
-echo "dongbuict0" | sudo -S cp ./extlinux.conf /boot/extlinux/extlinux.conf
+echo "dongbuict0" | sudo -S cp ~/init-tools/boot-ext-sdcard/extlinux.conf /boot/extlinux/extlinux.conf
